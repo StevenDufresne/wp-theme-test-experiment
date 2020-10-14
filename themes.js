709257a38f3db57e8552989b6d378c456b39be9d
dufresnesteven <steve.dufresne@automattic.com>
2020-10-14 12:05:51 +0900

    More tests.

--- themes.js
+++ themes.js
@@ -2,23 +2,59 @@ module.exports = [
 	{
 		name: 'colibri-wp',
 		url: 'https://downloads.wordpress.org/theme/colibri-wp.1.0.80.zip',
-    },
-    {
+	},
+	{
 		name: 'twentyfifteen',
 		url: 'https://downloads.wordpress.org/theme/twentyfifteen.2.7.zip',
-    },
-    {
-        name: 'arcade-basic',
-        url: 'https://wordpress.org/themes/download/arcade-basic.1.1.2.zip'
-    },
-    {
-        name: 'ratio-lite',
-        url: 'https://wordpress.org/themes/download/ratio-lite.1.3.zip'
-    },
-    {
-        name: 'storefront',
-        url: 'https://downloads.wordpress.org/theme/storefront.2.8.0.zip'
-    },
+	},
+	{
+		name: 'arcade-basic',
+		url: 'https://wordpress.org/themes/download/arcade-basic.1.1.2.zip',
+	},
+	{
+		name: 'ratio-lite',
+		url: 'https://wordpress.org/themes/download/ratio-lite.1.3.zip',
+	},
+	{
+		name: 'storefront',
+		url: 'https://downloads.wordpress.org/theme/storefront.2.8.0.zip',
+	},
+	{
+		name: 'oceanwp',
+		url: 'https://downloads.wordpress.org/theme/oceanwp.1.9.0.zip',
+	},
 
-    
+	{
+		name: 'twentysixteen',
+		url: 'https://downloads.wordpress.org/theme/twentysixteen.2.2.zip',
+	},
+
+	{
+		name: 'envo-online-store',
+		url:
+			'https://downloads.wordpress.org/theme/envo-online-store.1.0.4.zip',
+	},
+
+	{
+		name: 'mesmerize',
+		url: 'https://downloads.wordpress.org/theme/mesmerize.1.6.97.zip',
+	},
+
+	{
+		name: 'lyrical',
+		url: 'https://downloads.wordpress.org/theme/lyrical.1.1.3.zip',
+	},
+
+	{
+		name: 'vilva',
+		url: 'https://downloads.wordpress.org/theme/vilva.1.0.8.zip',
+	},
+	{
+		name: 'open-shop',
+		url: 'https://downloads.wordpress.org/theme/open-shop.1.1.7.zip',
+	},
+	{
+		name: 'catch-revolution',
+		url: 'https://downloads.wordpress.org/theme/catch-revolution.1.0.zip',
+	},
 ];
